677ba1d83a58b01898d412bb0890d477d4dff813
Alex Denton <alex.denton.dev@gmail.com>
2019-03-14 14:09:27 -0400

    Added GetAllDocuments to DocumentRepository

---
+++ DocumentApi/DocumentRepository.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocumentApi
+{
+    public class DocumentRepository
+    {
+        public Task<IEnumerable<DocumentDto>> GetAllDocuments()
+        {
+            var connection = new SqlConnection("fake-connection-string");
+            var command = connection.CreateCommand();
+
+            var commandText = @"
+                SELECT *
+                FROM Documents
+            ";
+
+            command.CommandText = commandText;
+
+            var documents = new List<DocumentDto>();
+
+            var reader = command.ExecuteReaderAsync().Result;
+            while (reader.ReadAsync().Result)
+            {
+                var document = new DocumentDto();
+                document.Id = (string)reader[0];
+                document.Name = (string)reader[1];
+                document.Content = (string)reader[2];
+                documents.Add(document);
+            }
+
+            return Task.FromResult(documents.AsEnumerable());
+        }
+    }
+}
---
+++ DocumentApi/DocumentsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DocumentApi
+{
+    public class DocumentsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("uploadDocuments")]
+        public Task<ActionResult<IEnumerable<DocumentDto>>> UploadDocuments()
+        {
+            var documentRepository = new DocumentRepository();
+
+            HttpContext.Response.StatusCode = 200;
+        }
+    }
+}
