74b4fde36db798108a433cee16249bec5e1f728d
toxicbakery <toxicbakery@gmail.com>
2019-08-22 14:58:15 -0500

    Making a clean PR of the code

---
+++ DocumentApi/DocumentTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace DocumentApi
+{
+    [TestFixture]
+    public class DocumentTests
+    {
+        [Test]
+        public void ShouldNotUploadDocumentsThatAlreadyExist()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void ShouldUploadDocumentsThatDoNotAlreadyExist()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This is an interview programming challenge. It is written poorly on purpose. Please don't judge me.
